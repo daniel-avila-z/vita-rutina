c28334f174727c4d0595d4fa6389efa105097eaa
Daniel Avila <roicesdaniel@gmail.com>
2023-04-04 22:46:15 -0400

    a ver

---
+++ src/app/products/page.jsx
@@ -0,0 +1,11 @@
+
+export default function Products () {
+    return (
+      <main className=''>
+        <div className=''>
+          <h1>Hola, Estas en Home</h1>
+        </div>
+      </main>
+    )
+  }
+  
