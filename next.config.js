ae1144c47d38e1ae55578aa886d8f0a91d3e3d74
Daniel Avila <roicesdaniel@gmail.com>
2023-04-07 22:27:31 -0400

    arreglando

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true
-    },
-    // images: {
-    //   domains: ['avatars.dicebear.com']
-    // },
-    reactStrictMode: true
-  }
-  
-  module.exports = nextConfig
+  experimental: {
+    appDir: true
+  },
+  // images: {
+  //   domains: ['avatars.dicebear.com']
+  // },
+  reactStrictMode: true
+}
+
+module.exports = nextConfig
