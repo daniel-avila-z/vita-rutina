c6075137e2f9ffa924cca44fc1281cf661e274b2
Daniel Avila <roicesdaniel@gmail.com>
2023-04-07 22:49:00 -0400

    arreglando

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,59 +1,7 @@
 'use client'
-// import React, { useState } from 'react'
-import '../styles/Navbar.css'
-
-// const Toggle = () => {
-//   const [activeCategory, setActiveCategory] = useState('mascarillas')
-//   const [activeSubCategory, setActiveSubCategory] = useState('')
-
-//   const handleCategoryClick = (category) => {
-//     if (activeCategory === category) {
-//       setActiveCategory('')
-//       setActiveSubCategory('')
-//     } else {
-//       setActiveCategory(category)
-//       setActiveSubCategory('')
-//     }
-//   }
-
-//   const handleSubCategoryClick = (subCategory) => {
-//     setActiveSubCategory(subCategory)
-//   }
-
-//   return (
-//     <div className='toggle-container'>
-//       <div className='toggle-category'>
-//         <h3 onClick={() => handleCategoryClick('mascarillas')}>Mascarillas</h3>
-//         {activeCategory === 'mascarillas' && (
-//           <ul className='toggle-subcategories'>
-//             <li onClick={() => handleSubCategoryClick('en polvo')}>En polvo</li>
-//             <li onClick={() => handleSubCategoryClick('en crema')}>En crema</li>
-//           </ul>
-//         )}
-//       </div>
-//       <div className='toggle-category'>
-//         <h3 onClick={() => handleCategoryClick('productos')}>Productos</h3>
-//         {activeCategory === 'productos' && (
-//           <ul className='toggle-subcategories'>
-//             <li onClick={() => handleSubCategoryClick('cremas')}>Cremas</li>
-//             <li onClick={() => handleSubCategoryClick('pinturas')}>Pinturas</li>
-//           </ul>
-//         )}
-//         {activeSubCategory === 'pinturas' && (
-//           <ul className='toggle-sub-subcategories'>
-//             <li onClick={() => handleSubCategoryClick('labios')}>Labios</li>
-//             <li onClick={() => handleSubCategoryClick('uñas')}>Uñas</li>
-//           </ul>
-//         )}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Toggle
-
 import React, { useState } from 'react'
 import Link from 'next/link'
+import '../styles/Navbar.css'
 
 export default function Navbar () {
   const [activeCategory, setActiveCategory] = useState('')
